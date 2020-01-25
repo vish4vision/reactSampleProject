bba2bc2a5ba4d323167b2b0192ab1107a91e8cd4
Vishal Taywade <vish4vision@gmail.com>
2020-01-25 12:36:21 +0530

    first commit

---
+++ react-deploy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hurrey Its deployed</h1>
+      
+    </div>
+  );
+}
+
+export default App;
